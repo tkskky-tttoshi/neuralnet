411c0053f69971db4a2f3ffe476a122f169e95df
tkskky-tttoshi <tkskky.naruto921@gmail.com>
2020-07-09 20:58:59 +0900

    make feed-forward of system1

--- main.py
+++ main.py
@@ -1,10 +1,94 @@
+#coding: UTF-8
+from neuralnet import *
+from matplotlib import pyplot as plt
+
+class System1:
+	
+	def __init__(self, k):
+		self.__k = k
+
+	#システム出力の目標値
+	def __calculate_system_target(self):
+		#2*pi*k/50が正なら1，下回った場合-1を格納する
+		x=lambda i : math.sin(2*math.pi*i/50)
+		self.__r_data_set=[(x(i)>=0)-(x(i)<0) for i in range(self.__k)]
+
+	#システム出力
+	def __calculate_system_output(self, y1, y2, u1, u2):
+		a1, a2 = -1, 0.35 
+		b0, b1 = 1, 0.75
+		return -a1*y1-a2*y2+b0*u1+b1*u2
+
+	#システム出力の誤差
+	def __calculate_error(self, r, y):
+		return r-y
+
+	#システム入力
+	def __calculate_system_input(self, u1, e, e1, e2):
+		Kp, Ki, Kd = 0.5, 0.1, 0.05
+		return u1+Kp*(e-e1)+Ki*e+Kd*(e-2*e1+e2)
 
 
-#def __make_inputs(self):
-#        u=np.zeros(1000)
-#        y=np.zeros(1000)
-        
+	#入力データuと教師データyの取得
+	def make_u_y_data_set(self):
+		u1, u2, y, y1, y2, e1, e2= 0, 0, 0, 0, 0, 0, 0
+		self.__calculate_system_target()
+		self.__u_data_set=np.zeros(self.__k)
+		self.__y_data_set=np.zeros(self.__k)
 
- #システムの出力誤算
-    def __system_error(self, training_data_y, target):
-        return target-training_data_y
+		for i in range(self.__k):
+			r=self.__r_data_set[i]
+			e=self.__calculate_error(r, y)
+			u=self.__calculate_system_input(u1, e, e1, e2)
+			self.__y_data_set[i] = y
+			self.__u_data_set[i] = u
+
+			#y1, y2, u1, u2の更新
+			u2 = u1
+			u1 = u
+			y2 = y1
+			y1 = y
+			y = self.__calculate_system_output(y1, y2, u1, u2)
+			e2 = e1
+			e1 = e
+
+	def make_neuralnet_input_data(self):
+		self.__modified_y_data_set=np.repeat(self.__y_data_set,2)[1:-1].reshape((-1, 2))
+		self.__modified_u_data_set=np.repeat(self.__u_data_set,2)[1:-1].reshape((-1, 2))
+		self.__input_data=np.c_[self.__modified_y_data_set, self.__modified_u_data_set]
+
+	@property
+	def y_data_set(self):
+		return self.__y_data_set
+
+	@property
+	def u_data_set(self):
+		return self.__u_data_set
+
+	@property
+	def r_data_set(self):
+		return self.__r_data_set
+	
+	@property
+	def k(self):
+		return self.__k
+	
+	@property
+	def input_data(self):
+		return self.__input_data
+
+
+if __name__ =="__main__":
+
+	system1=System1(1000)
+	system1.make_u_y_data_set()
+	system1.make_neuralnet_input_data()
+	print(system1.input_data)
+	
+	neuralnet=NeuralNet(4,10)
+	neuralnet.train(system1.y_data_set, system1.input_data,eta=0.2)
+
+	neuralnet.make_graph(system1.y_data_set, neuralnet.y_data_set)
+
+
+
--- neuralnet.py
+++ neuralnet.py
@@ -2,19 +2,22 @@
 import numpy as np
 import math 
 import random
+from matplotlib import pyplot as plt
+
 	
 class NeuralNet:
     #コンストラクタ
 
 	def __init__(self, number_of_input, number_of_hidden):
-		self.hidden_weight=np.random.random_sample((number_of_input,number_of_hidden))
+		self.hidden_weight=np.random.random_sample((number_of_hidden,number_of_input))
 		self.output_weight=np.random.random_sample(number_of_hidden)
+
 
 	# def __sigmoid(self, array):
 	# 	return np.vectorize(lambda x: 1.0/(1.0+math.exp(-x)))(array)
 
 	#シグモイド関数
-	def __sigmoid(self,x):
+	def __sigmoid(self, x):
 		return 1/(1+np.exp(-x))
 	#シグモイド関数の微分
 	def __sigmoid_derivative(self, x):
@@ -22,56 +25,102 @@ class NeuralNet:
 		return sigmoid*(1.0-sigmoid)
 
 	#隠れ層での活性化関数
-	def activation_on_hidden(self,X):
-		vec=np.vectorize(np.dot)(X, self.hidden_weight)
-		return np.vectorize(self.__sigmoid)(vec)
+	def __activation_on_hidden(self,X):
+		vector=np.dot(self.hidden_weight,X)
+		return np.vectorize(self.__sigmoid)(vector)
 
 	#出力層での活性化関数
-	def activation_on_output(self,V):
+	def __activation_on_output(self,V):
 		return np.dot(V, self.output_weight)
 
 	#損失関数
-	def __loss(self, training_data, V):
-		return self.__activation_on_output(V)-training_data
+	def __loss(self, training_data, y):
+		return training_data-y
 
-	def __update_w2_weight(self, training_data, V, eta):
-		self.error=self.__loss(learning_data, V)
+
+	#隠れ層の重みの更新
+	def __update_output_weight(self, training_data, V, eta, error):
 		f_dot=1
+	
 		dj_dw=error*f_dot*V
 		self.output_weight=self.output_weight+eta*dj_dw
 
-	def update_w1_weight(self, training_data, X, V, eta):
-		# error=self.error
-		error=0.1
-		f_dot1=np.vectorize(self.__sigmoid_derivative)(X,self.hidden_weight)
-		print(f_dot1.size())
-		f_dot2=1
-		dj_dw=error*f_dot1*self.output_weight*f_dot2*X
+		#print("")
+		#print("Updating Weight between Output Layer and Hidden Layer")
+		# print("---Output Weight---")
+		# print(self.output_weight)
 
-		return W1+eta*dj_dw
+	#出力層の重みの更新
+	def __update_hidden_weight(self, training_data, X, V, eta, error):
+		#print("")
+		#print("Updating Weight between Input and Hidden Layer")
 
-	#def train(self,x,training_data):
+		f_dot_hidden=np.dot(self.hidden_weight,X)
+		f_dot_hidden=np.vectorize(self.__sigmoid_derivative)(f_dot_hidden)
+		f_dot_output=1
+
+		for i in range(self.output_weight.size):
+			w=self.output_weight[i]
+			f_dot_hidden_vector=f_dot_hidden[i]
+			
+			dj_dw=error*f_dot_hidden_vector*w*f_dot_output*X
+			updated_weight=eta*dj_dw
+		
+			self.hidden_weight=np.delete(self.hidden_weight,i,0)
+			self.hidden_weight=np.insert(self.hidden_weight,i,updated_weight,axis=0)
+			
+
+	def __back_propagation(self,training_data, X, V, eta, error):
+		self.__update_output_weight(training_data, V, eta,error)
+		self.__update_hidden_weight(training_data, X, V, eta,error)
+
+	def forward(self, X):
+		self.__V=self.__activation_on_hidden(X)
+		return self.__activation_on_output(self.__V)
+
+
+	def train(self, training_data_set, X_data_set, eta):
+		i=0
+		data_size=training_data_set.size
+		self.y_data_set=np.zeros(data_size)
+		for (X, training_data, i) in zip(X_data_set, training_data_set, range(data_size)):
+			print("")
+			print("")
+			print("**********")
+			print(str(i+1)+"回目")
+
+			y=self.forward(X)
+			self.y_data_set[i]=y
+			print("Output y_nn")
+			print(y)
+			print("Output y")
+			print(training_data)
+			error=self.__loss(training_data,y)
+			print("")
+			print("===ERROR===")
+			print(error)
+			self.__back_propagation(training_data, X, self.__V, eta, error)
+
+	def make_graph(self, training_data_set, y_data_set):
+		x=range(0, 100)
+		y=training_data_set[:100]
+		y_nn=y_data_set[:100]
+		plt.plot(x,y, x, y_nn)
+		plt.show()
+
 
 
 
 if __name__=="__main__":
-	neuralnet=NeuralNet(2,4)
-	print("hidden")
-	print(neuralnet.hidden_weight)
-	print("output")
-	print(neuralnet.output_weight)
-	V=np.arange(8).reshape(2,4)
-	print(V)
-	print("hidden_updated")
-	print(neuralnet.activation_on_hidden(V))
-	print("output_updated")
-	X=np.arange(4)
-	print(neuralnet.activation_on_output(X))
-	training_data=1
-	print(neuralnet.update_w1_weight(training_data,X,V,eta=0.1))
-
-
-  
+	neuralnet=NeuralNet(4,5)
+			
+	print("START")
+	
+  	X_data_set=np.random.rand(200).reshape(50,4)
+  	training_data_set=np.random.rand(50)
+  	print(X_data_set)
+  	print(training_data_set)
+  	neuralnet.train(training_data_set, X_data_set, eta=0.2)
 
 
 
